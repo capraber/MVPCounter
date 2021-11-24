7428223c417e2e85e9f9271203c70f0d9e44cb85
Mauro Sarti <mauro.sarti@globant.com>
2021-11-24 13:27:08 -0300

    MVP update
    Tested in: Samsung S10+
    Signed off by: Mauro Sarti mauro.sarti@globant.com

--- app/src/main/java/com/globant/mvpcounter/mvp/model/CountModel.java
+++ app/src/main/java/com/globant/mvpcounter/mvp/model/CountModel.java
@@ -1,17 +1,22 @@
 package com.globant.mvpcounter.mvp.model;
 
-public class CountModel {
+import com.globant.mvpcounter.mvp.contract.CountContract;
+
+public class CountModel implements CountContract.Model {
 
     private int count = 0;
 
+    @Override
     public void reset() {
         count = 0;
     }
 
+    @Override
     public void inc() {
         count += 1;
     }
 
+    @Override
     public int getCount() {
         return count;
     }
--- app/src/main/java/com/globant/mvpcounter/mvp/view/CountView.java
+++ app/src/main/java/com/globant/mvpcounter/mvp/view/CountView.java
@@ -3,17 +3,32 @@ package com.globant.mvpcounter.mvp.view;
 import android.app.Activity;
 
 import com.globant.mvpcounter.databinding.ActivityMainBinding;
+import com.globant.mvpcounter.mvp.contract.CountContract;
+import com.globant.mvpcounter.mvp.view.base.ActivityView;
 
-public class CountView extends ActivityView {
+public class CountView extends ActivityView implements CountContract.View {
 
     private final ActivityMainBinding binding;
 
-    public CountView(Activity activity, ActivityMainBinding binding) {
+    public CountView(Activity activity) {
         super(activity);
-        this.binding = binding;
+
+        binding = ActivityMainBinding.inflate(activity.getLayoutInflater());
+        activity.setContentView(binding.getRoot());
     }
 
+    @Override
     public void setCount(String count) {
         binding.countLabel.setText(count);
+    }
+
+    @Override
+    public void onCountButtonPressed(Runnable onClick) {
+        binding.countButton.setOnClickListener(v -> onClick.run());
+    }
+
+    @Override
+    public void onResetButtonPressed(Runnable onClick) {
+        binding.resetButton.setOnClickListener(v -> onClick.run());
     }
 }
